2d33f4e25055acfe5e8c2a21c143b8465f4e5664
meet <wdwmq@sinhgad.edu>
2026-04-02 00:55:13 +0530

    fix: use @vladmandic/face-api and add supabase dependency support

--- src/Drowsiness.js
+++ src/Drowsiness.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import * as faceapi from "face-api.js";
+import * as faceapi from "@vladmandic/face-api";
 import { supabase } from "./supabase";
 
 
